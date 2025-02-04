c51f79b168a1ba254def4bf0d18f90244bbcaf58
jeanport <pierreanatc522@gmail.com>
2025-02-03 21:43:01 -0300

    Renomeando classe HandleExcption

--- src/main/java/com/jean/api_transacao/exception/HandleException.java
+++ src/main/java/com/jean/api_transacao/exception/HandleException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class HandleExcption {
+public class HandleException {
 
 
     @ExceptionHandler(DataInvalidaException.class)
