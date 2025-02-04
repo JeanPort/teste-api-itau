fb47cb1dc4aaf746e87bb910974a09f42e3e40a8
jeanport <pierreanatc522@gmail.com>
2025-02-03 21:41:58 -0300

    Criando classe HandleException

---
+++ src/main/java/com/jean/api_transacao/exception/HandleExcption.java
@@ -0,0 +1,26 @@
+package com.jean.api_transacao.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class HandleExcption {
+
+
+    @ExceptionHandler(DataInvalidaException.class)
+    public ResponseEntity<Void> handlerDataInvalideException(DataInvalidaException e) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+    }
+
+    @ExceptionHandler(JsonIncorretoExcption.class)
+    public ResponseEntity<Void> handlerJsonIncorretoExcption(JsonIncorretoExcption e) {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(ValorIncorretoException.class)
+    public ResponseEntity<Void> handlerValorIncorretoException(ValorIncorretoException e) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+    }
+}
