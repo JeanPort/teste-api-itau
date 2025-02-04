fefb2581ecf2dd184046c93985e1eb2e35f98d29
jeanport <pierreanatc522@gmail.com>
2025-02-03 21:51:45 -0300

    Concluindo funçao create no controller

---
+++ src/main/java/com/jean/api_transacao/controller/TransacaoController.java
@@ -0,0 +1,25 @@
+package com.jean.api_transacao.controller;
+
+import com.jean.api_transacao.dto.TransacaoDTO;
+import com.jean.api_transacao.service.ITransacaoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TransacaoController {
+
+    private final ITransacaoService service;
+
+    public TransacaoController(ITransacaoService service) {
+        this.service = service;
+    }
+
+    @PostMapping("transacao")
+    public ResponseEntity<Void> create(@RequestBody TransacaoDTO dto) {
+        service.create(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
